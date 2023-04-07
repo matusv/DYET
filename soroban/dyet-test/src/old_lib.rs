ac5000f00688b9393583549ec8ba934f8c23731d
Matus <matus@gmail.com>
2023-04-07 17:01:16 +0200

    lib.rs refactor

--- soroban/dyet-test/src/old_lib.rs
+++ soroban/dyet-test/src/old_lib.rs
@@ -7,26 +7,9 @@ const NUM_FNS: Symbol = symbol!("NUM_FNS");
 const FEATURES: Symbol = symbol!("FEATURES");
 const COVERAGE: Symbol = symbol!("COVERAGE");
 
-fn get_coverage_from_storage(env: &Env) -> Vec<u32> {
-    match env.storage().get(&COVERAGE) {
-        Some(Ok(c)) => c,
-        _ => Vec::new(&env),
-    }
-}
-
-fn get_features_from_storage(env: &Env) -> Vec<u32> {
-    match env.storage().get(&FEATURES) {
-        Some(Ok(features)) => features,
-        _ => Vec::new(&env),
-    }
-}
-
-fn update_coverage(env: &Env, cs_copy: &Vec<u32>) {
-    env.storage().set(&COVERAGE, cs_copy);
-}
-
 #[contractimpl]
 impl Contract {
+
     pub fn init(env: Env, num_fns: u32, features: Vec<u32>) -> (u32, Vec<u32>, Vec<u32>) {
         env.storage().set(&NUM_FNS, &num_fns);
         env.storage().set(&FEATURES, &features);
@@ -37,22 +20,50 @@ impl Contract {
         for _ in 0..coverage_n {
             covered_blocks.push_back(0);
         }
-        update_coverage(&env, &covered_blocks);
+        env.storage().set(&COVERAGE, &covered_blocks);
 
         (num_fns, features, covered_blocks)
     }
 
     pub fn coverage(env: Env) -> Vec<u32> {
-        get_coverage_from_storage(&env)
+        let cs: Vec<u32> = match env.storage().get(&COVERAGE) {
+            Some(Ok(c)) => c,
+            _ => Vec::new(&env),
+        };
+        cs
+
+        // let mut sum: u32 = 0;
+        
+        // for i in 0..cs.len() {
+        //     sum += cs.get(i).unwrap_or(Ok(0)).unwrap();
+        // }
+        // sum
     }
 
     pub fn features(env: Env) -> Vec<u32> {
-        get_features_from_storage(&env)
+        let fs: Vec<u32> = match env.storage().get(&FEATURES) {
+            Some(Ok(c)) => c,
+            _ => Vec::new(&env),
+        };
+        fs
     }
 
     pub fn call(env: Env, fn_i: u32, arg: u32) -> Vec<u32> {
-        let fs = get_features_from_storage(&env);
-        let cs = get_coverage_from_storage(&env);
+        // let num_fns: u32 = env
+        //     .storage()
+        //     .get(&NUM_FNS)
+        //     .unwrap_or(Ok(0))
+        //     .unwrap();
+
+        let fs: Vec<u32> = match env.storage().get(&FEATURES) {
+            Some(Ok(features)) => features,
+            _ => Vec::new(&env),
+        };
+
+        let cs: Vec<u32> = match env.storage().get(&COVERAGE) {
+            Some(Ok(c)) => c,
+            _ => Vec::new(&env),
+        };
 
         let mut cs_copy: Vec<u32> = Vec::new(&env);
         for i in 0..cs.len() {
@@ -69,7 +80,7 @@ impl Contract {
             cs_copy.set(index1, 1)
         }
         
-        update_coverage(&env, &cs_copy);
+        env.storage().set(&COVERAGE, &cs_copy);
 
         cs_copy
     }
